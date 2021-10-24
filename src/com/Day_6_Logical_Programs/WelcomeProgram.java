0f36cdc93053f952e276061d010c949e3d067899
“ArafathvBaig” <arafathbaig1997@gmail.com>
2021-10-24 19:48:08 +0530

    Adding Welcome File

---
+++ src/com/Day_6_Logical_Programs/WelcomeProgram.java
@@ -0,0 +1,10 @@
+package com.Day_6_Logical_Programs;
+
+public class WelcomeProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Day 6 Logical Programming");
+
+	}
+
+}
